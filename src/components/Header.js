86275cae860841c647d399daa27dcda5af116bd3
Bertrand Bouland <bbformationcefim@gmail.com>
2021-06-25 17:26:28 +0200

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class Header extends React.Component {
+    render() {
+        return (
+            <header className="bg-indigo-400 text-white" >
+                <div className="container flex items-center justify-between mx-auto h-24">
+                <div id="logo" className="">
+                    TOUITEUR LE RETOUR
+                </div>
+                <h1 className="text-3xl">
+                    Parce que c'est l'heure de touiter again
+                </h1>
+                </div>
+            </header>
+        )
+    }
+}
+
+export default Header;
